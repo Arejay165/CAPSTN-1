c7ede3c9b9654b069f452163d405d1e45ad4e098
Dijay165 <arjay.diocton@gmail.com>
2021-10-15 03:06:44 +0800

    Scriptable Object Tutorial Instruction Text Template

---
+++ Assets/Scripts/TutorialUI/TutorialTexts.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "New Tutorial Script", menuName = "TutsScript")]
+public class TutorialTexts : ScriptableObject
+{
+    public List<string> instructions;
+}
